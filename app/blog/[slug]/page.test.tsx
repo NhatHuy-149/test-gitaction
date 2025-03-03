381fc6a79f7a1976d0ef1f150a25968b9d1b5027
NhatHuy-149 <nhathuydoan1409@gmail.com>
2025-03-03 17:56:04 +0700

    update

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -9,3 +9,8 @@ it("App Router: Works with dynamic route segments", () => {
   render(<Page params={{ slug: "Test" }} />);
   expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
 });
+
+it("Nhathuy should be in the document",() => { 
+  render(<Page params={{slug:"Test"}}/>)
+  expect(screen.getByText("Nhathuy")).toBeInTheDocument()
+ })
